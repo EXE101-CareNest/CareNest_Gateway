c1b133811d43e59dfbab6807fdf55e244da1a5bd
Đông Nghi <hoangvodonghi@gmail.com>
2025-09-03 08:46:49 +0700

    change to .net

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm file cấu hình ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Đăng ký dịch vụ Ocelot
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+// Dùng Ocelot làm gateway
+app.UseOcelot().Wait();
+
+app.Run();
