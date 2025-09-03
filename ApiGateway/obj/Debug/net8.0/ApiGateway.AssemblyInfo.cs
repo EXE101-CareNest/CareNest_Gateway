4cbd2ca4e6fef6893e4b480f091235e2fc0affbe
Đông Nghi <hoangvodonghi@gmail.com>
2025-09-03 09:38:26 +0700

    update docker

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285991bed2de553fbc3d6a2c5a281e50e071c52c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b133811d43e59dfbab6807fdf55e244da1a5bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
