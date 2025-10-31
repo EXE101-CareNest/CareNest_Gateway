e34aa0a7662db8cb66abb38a64775d07471654ff
Đông Nghi <hoangvodonghi@gmail.com>
2025-11-01 06:46:41 +0700

    update deploy

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa30b30c4ec98950a501408ff4a437d63edf7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414e7a575b2688297facff05d3ec36f2742c88b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
